5fb540d74d52e0295ef43ca167991b1722f42666
samyak <samyak@techuz.com>
2026-02-18 15:09:00 +0530

    cleaning

--- src/server.js
+++ src/server.js
@@ -3,11 +3,7 @@ dotenv.config();
 
 import http from "http";
 import app from "./app.js";
-import { initAudioSocket } from "./ws/audio.socket.js";
-import { initDynamicAudioSocket } from "./ws/dynamicAudio.socket.js";
-import { handleSpeechMatrixConnection } from "./ws/speechMatrix.js";
-import { initPythonSocket } from "./ws/python.socket.js";
-import { handleSpeechMatrixConnectionSession } from "./ws/sessionSocket.js";
+
 import { handleSessionBackendConnection } from "./ws/modifySessionSocket.js";
 import { handleConversationWaiterConnection } from "./ws/conversationSocket.js";
 
@@ -15,34 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-// initAudioSocket(server);
-// initDynamicAudioSocket(server);
-// initPythonSocket(server);
-// handleSpeechMatrixConnection(server);
-// handleSpeechMatrixConnectionSession(server);
 // handleSessionBackendConnection(server);
 handleConversationWaiterConnection(server);
 server.listen(PORT, () => {
   console.log(`🚀 Backend running on port ${PORT}`);
 });
-
-
-//---------------------------------------------------------------working
-// import http from "http";
-// import app from "./app.js";
-// import { initSocket } from "./socket/socket.server.js";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-
-// const PORT = process.env.PORT || 3000;
-
-// const server = http.createServer(app);
-
-// initSocket(server);
-
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-//---------------------------------------------------------------working
