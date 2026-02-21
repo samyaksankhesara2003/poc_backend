9857a9808554238956da17dec733d256acb47a57
samyak <samyak@techuz.com>
2026-02-21 15:15:45 +0530

    cleanup thinks

--- src/server.js
+++ src/server.js
@@ -19,24 +19,3 @@ handleSpeechMatrixConnection(server);
 server.listen(PORT, () => {
   console.log(`🚀 Backend running on port ${PORT}`);
 });
-
-
-//---------------------------------------------------------------working
-// import http from "http";
-// import app from "./app.js";
-// import { initSocket } from "./socket/socket.server.js";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-
-// const PORT = process.env.PORT || 3000;
-
-// const server = http.createServer(app);
-
-// initSocket(server);
-
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-//---------------------------------------------------------------working
--- src/services/realtime.service.js
+++ src/services/realtime.service.js
@@ -4,36 +4,6 @@ import { exec } from "child_process";
 import { promisify } from "util";
 
 const execAsync = promisify(exec);
-
-//  const transcribeChunk = async (chunk) => {
-//   const baseDir = "realtime";
-//   const outDir = "realtime_outputs";
-
-//   if (!fs.existsSync(baseDir)) fs.mkdirSync(baseDir);
-//   if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
-
-//   const timestamp = Date.now();
-//   const webmPath = path.join(baseDir, `${timestamp}.webm`);
-//   const wavPath = path.join(baseDir, `${timestamp}.wav`);
-
-//   // Save chunk
-//   fs.writeFileSync(webmPath, Buffer.from(chunk));
-
-//   // Convert to wav
-//   await execAsync(
-//     `ffmpeg -i "${webmPath}" -ar 16000 -ac 1 -c:a pcm_s16le "${wavPath}"`
-//   );
-
-//   // Whisper
-//   await execAsync(
-//     `/home/techuz/.local/bin/whisper "${wavPath}" --model small --language en --output_format txt --output_dir ${outDir}`
-//   );
-
-//   const txtPath = path.join(outDir, `${timestamp}.txt`);
-//   if (!fs.existsSync(txtPath)) return "";
-
-//   return fs.readFileSync(txtPath, "utf8").trim();
-// };
 const transcribeChunk = async (arrayBuffer) => {
     const baseDir = "realtime";
     const outDir = "realtime_outputs";
@@ -71,13 +41,8 @@ const transcribeChunk = async (arrayBuffer) => {
         return "";
       }
   
-      const text = fs.readFileSync(txtPath, "utf8").trim();
+      const text = fWs.readFileSync(txtPath, "utf8").trim();
       
-      // Optional: Clean up files
-      // fs.unlinkSync(webmPath);
-      // fs.unlinkSync(wavPath);
-      // fs.unlinkSync(txtPath);
-  
       return text;
     } catch (error) {
       console.error('Transcription error:', error.message);
